7e8607e6511dbf2b2c7d36db4e16765a1c09b131
chandlery54 <chandlery54@gmail.com>
2024-12-16 09:31:47 -0700

    Add Footer and old home page

---
+++ lib/footer.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class Footer extends StatelessWidget {
+  const Footer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Theme.of(context).colorScheme.inverseSurface,
+      child: Center(
+        child: Container(
+          constraints: const BoxConstraints(
+            minWidth: 300,
+            maxWidth: 1000,
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  _FooterText('B.J.C. News'),
+                  Divider(height: 24.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      _FooterText(
+                          '© Copyright 2024 B.J.C. News.  All Rights Reserved'),
+                      Row(
+                        children: [
+                          _FooterText('Terms of Service'),
+                          const SizedBox(width: 24.0),
+                          _FooterText('Privacy Policy'),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _FooterText extends StatelessWidget {
+  const _FooterText(
+    this.text, {
+    super.key,
+  });
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: Theme.of(context)
+          .textTheme
+          .bodyMedium
+          ?.copyWith(color: Theme.of(context).colorScheme.onInverseSurface),
+    );
+  }
+}
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:bjc_games/footer.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -8,112 +9,123 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        titleTextStyle: Theme.of(context).textTheme.displayLarge,
+        titleTextStyle: Theme.of(context).textTheme.displaySmall,
+        centerTitle: false,
         // backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(title),
       ),
-      body: ListView(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          // Padding(
-          //   padding: const EdgeInsets.all(16.0),
-          //   child: Center(
-          //     child: Text(
-          //       title,
-          //       style: Theme.of(context)
-          //           .textTheme
-          //           .displayLarge
-          //           ?.copyWith(fontSize: 120.0),
-          //     ),
-          //   ),
-          // ),
-          Center(
-            child: Container(
-              constraints: const BoxConstraints(
-                minWidth: 300,
-                maxWidth: 600,
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 100.0),
-                    child: Text(
-                      'Play some of our favorite games!',
-                      style: Theme.of(context).textTheme.headlineMedium,
-                      textAlign: TextAlign.center,
-                    ),
+          ListView(
+            shrinkWrap: true,
+            children: [
+              // Padding(
+              //   padding: const EdgeInsets.all(16.0),
+              //   child: Center(
+              //     child: Text(
+              //       title,
+              //       style: Theme.of(context)
+              //           .textTheme
+              //           .displayLarge
+              //           ?.copyWith(fontSize: 120.0),
+              //     ),
+              //   ),
+              // ),
+              Center(
+                child: Container(
+                  constraints: const BoxConstraints(
+                    minWidth: 300,
+                    maxWidth: 600,
                   ),
-                  Card(
-                    margin: const EdgeInsets.all(24.0),
-                    clipBehavior: Clip.hardEdge,
-                    child: InkWell(
-                      onTap: () async {
-                        // TODO Navigate to 2048 Game
-                      },
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Container(
-                            height: 256,
-                            decoration: BoxDecoration(
-                              image: DecorationImage(
-                                image: AssetImage(
-                                    'assets/images/2048_screenshot.png'),
-                                // fit: BoxFit.fitWidth,
-                                alignment: Alignment.topCenter,
-                              ),
-                              // color: Colors.orange,
-                            ),
-                            // child: Center(
-                            //   child: Container(color: Colors.blue),
-                            // ),
-                          ),
-                          const SizedBox(height: 16.0),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                '2048',
-                                style: Theme.of(context).textTheme.displaySmall,
-                              ),
-                            ],
-                          ),
-                          const SizedBox(height: 20.0),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              OutlinedButton(
-                                onPressed: () async {
-                                  // TODO Navigate to 2048 Game
-                                },
-                                child: Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                    vertical: 8.0,
-                                    horizontal: 60.0,
-                                  ),
-                                  child: Text(
-                                    'Play',
-                                    style:
-                                        Theme.of(context).textTheme.titleLarge,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                          const SizedBox(height: 20.0),
-                        ],
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 100.0),
+                        child: Text(
+                          'Play some of our favorite games!',
+                          style: Theme.of(context).textTheme.headlineMedium,
+                          textAlign: TextAlign.center,
+                        ),
                       ),
-                    ),
+                      Card(
+                        margin: const EdgeInsets.all(24.0),
+                        clipBehavior: Clip.hardEdge,
+                        child: InkWell(
+                          onTap: () async {
+                            // TODO Navigate to 2048 Game
+                          },
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Container(
+                                height: 256,
+                                decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                    image: AssetImage(
+                                        'assets/images/2048_screenshot.png'),
+                                    // fit: BoxFit.fitWidth,
+                                    alignment: Alignment.topCenter,
+                                  ),
+                                  // color: Colors.orange,
+                                ),
+                                // child: Center(
+                                //   child: Container(color: Colors.blue),
+                                // ),
+                              ),
+                              const SizedBox(height: 16.0),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    '2048',
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .displaySmall,
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(height: 20.0),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  OutlinedButton(
+                                    onPressed: () async {
+                                      // TODO Navigate to 2048 Game
+                                    },
+                                    child: Padding(
+                                      padding: const EdgeInsets.symmetric(
+                                        vertical: 8.0,
+                                        horizontal: 60.0,
+                                      ),
+                                      child: Text(
+                                        'Play',
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .titleLarge,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(height: 20.0),
+                            ],
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 100.0),
+                      // Text(
+                      //   'More games coming soon!',
+                      //   style: Theme.of(context).textTheme.headlineSmall,
+                      // )
+                    ],
                   ),
-                  const SizedBox(height: 100.0),
-                  // Text(
-                  //   'More games coming soon!',
-                  //   style: Theme.of(context).textTheme.headlineSmall,
-                  // )
-                ],
+                ),
               ),
-            ),
+            ],
           ),
+          const Footer(),
         ],
       ),
     );
