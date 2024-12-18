fa418dd9149c2c29bbcc38ea7ab24f5f35013c15
chandlery54 <chandlery54@gmail.com>
2024-12-17 23:31:47 -0700

    Update color scheme and home page

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,10 @@ class BjcGamesApp extends StatelessWidget {
       title: 'BJC Games',
       theme: ThemeData(
         fontFamily: 'Grandstander',
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueGrey),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blueGrey,
+          dynamicSchemeVariant: DynamicSchemeVariant.fidelity,
+        ),
         useMaterial3: true,
       ),
       home: const HomePage(title: 'B.J.C. Games'),
